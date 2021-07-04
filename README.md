f028c3a3940877ac3be94c8340e4d1006f30ef7a
Leonardo T. Fernandes <leo.80@protonmail.com>
2021-07-04 19:48:36 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Strapi application
+
+Strapi api using documentation and GraphQL plugins for study purposes. 
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1d60ab21-1baf-4705-b20a-c151d951101b'
+};
